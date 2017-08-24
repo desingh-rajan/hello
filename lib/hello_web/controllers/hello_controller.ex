509d1bfe0907df4b3a7e2c265fb24f7ed89831ce
Desinghrajan <desingh@elumitas.com>
2017-08-24 18:26:49 +0530

    Initital commit

---
+++ lib/hello_web/controllers/hello_controller.ex
@@ -0,0 +1,12 @@
+defmodule HelloWeb.HelloController do
+  use HelloWeb, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+
+  def show(conn, %{"user" => user}) do
+    render conn, "show.html", user: user
+  end
+end
+
